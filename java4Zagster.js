e2145a7fdf4a08ecafc9a4711b0f7350544914e5
Unknown <stuart.withrow1@gmail.com>
2018-11-18 13:13:24 -0800

    Found the issue!

--- java4Zagster.js
+++ java4Zagster.js
@@ -1,14 +1,11 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-
 $(updateView)
 
 function updateView() {
-    $.getJSON(BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count", (UpdateRideCount)) //Prints most current number of rides.
 }
-
-function updateRideCount(data) {
+function UpdateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
 }
-
