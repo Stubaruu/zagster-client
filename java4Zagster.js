2e84ed15a66f2ab2d16117632fc62b0cd61270c7
Unknown <stuart.withrow1@gmail.com>
2018-11-18 15:01:58 -0800

    Playing with building nodes.

--- java4Zagster.js
+++ java4Zagster.js
@@ -5,6 +5,7 @@ $(updateView)
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", UpdateRideCount) //Prints most current number of rides.
     $.getJSON(BASE_URL + "/rides/count/per_month", UpdateMonthlyCount)
+    $.getJSON(BASE_URL + "",)
 }
 
 function UpdateRideCount(data) {
@@ -15,3 +16,24 @@ function UpdateRideCount(data) {
 function UpdateMonthlyCount(data) {
     console.log(data)
 }
+
+var vis = d3.select("#graph")
+            .append("svg");
+            var w = 900,
+            h = 400;
+    vis.attr("width", w)
+           .attr("height", h);
+    vis.text("The Graph")
+           .select("#graph")
+    vis.selectAll("circle .nodes")
+    .data(nodes)
+    .enter()
+    .append("svg:circle")
+    .attr("class", "nodes")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", "10px")
+    .attr("fill", "black") 
+var nodes = [{x: 30, y: 50},
+        {x: 50, y: 80},
+        {x: 90, y: 120}]
