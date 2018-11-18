f1048062f83d24b8df3b656bc2e53ef65f1d8c29
Unknown <stuart.withrow1@gmail.com>
2018-11-18 15:08:35 -0800

    Implementing visualization

--- java4Zagster.js
+++ java4Zagster.js
@@ -16,24 +16,22 @@ function UpdateRideCount(data) {
 function UpdateMonthlyCount(data) {
     console.log(data)
 }
-
+//DATA VISUALIZATION 
 var vis = d3.select("#graph")
-            .append("svg");
-            var w = 900,
-            h = 400;
-    vis.attr("width", w)
-           .attr("height", h);
-    vis.text("The Graph")
-           .select("#graph")
-    vis.selectAll("circle .nodes")
-    .data(nodes)
-    .enter()
-    .append("svg:circle")
-    .attr("class", "nodes")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", "10px")
-    .attr("fill", "black") 
-var nodes = [{x: 30, y: 50},
-        {x: 50, y: 80},
-        {x: 90, y: 120}]
+            .append("svg")
+.attr("width", 200).attr("height", 200);
+
+var nodes = [
+    {x: 10, y: 50},
+    {x: 70, y: 10},
+    {x: 140, y: 50}   
+  ]
+
+vis.selectAll("circle.nodes")
+   .data(nodes)
+   .enter()
+   .append("svg:circle")
+   .attr("cx", function(d) { return d.x; })
+   .attr("cy", function(d) { return d.y; })
+   .attr("r", "10px")
+   .attr("fill", "black")
