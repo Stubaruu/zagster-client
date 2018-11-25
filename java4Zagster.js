6222dc881fc6df7607c6c5b2c280638c7d4b3a3c
Unknown <stuart.withrow1@gmail.com>
2018-11-25 10:20:56 -0800

    Altering css and html. Format for website fixed.

--- java4Zagster.js
+++ java4Zagster.js
@@ -1,5 +1,14 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+const YEAR_RF = "/rides/count/g5"
+const STATION_RF = "/rides/count/columbia_simpson"
+const DESTINATION_RF = "/rides/count/drake_park"
+const USER_RF = "/rides/count/galveston"
+const MONTH_RF= "/rides/count/grc"
+const WEEK_RF = "/rides/count/old_mill"
+const DAY_RF = "/rides/count/"
+const TIME_RF = "/rides/count/TIME"
+
 $(updateView)
 
 function updateView() {
@@ -17,4 +26,3 @@ function UpdateMonthlyCount(data) {
     console.log(data)
 }
 //DATA VISUALIZATION 
-
